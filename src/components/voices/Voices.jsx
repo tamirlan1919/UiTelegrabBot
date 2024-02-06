170f694f5a1b084a16b9aaa3ecf50b21b337a51d
Timerlan Chinchaev <tchinchaev@bk.ru>
2024-02-07 01:05:19 +0300

    second

--- src/components/voices/Voices.jsx
+++ src/components/voices/Voices.jsx
@@ -1,68 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import style from './voices.module.css'
-import { GrCaretNext } from "react-icons/gr";
-import { IoCheckmarkDoneSharp } from "react-icons/io5";
-
+import style from './voices.module.css';
+import { GrCaretNext } from 'react-icons/gr';
+import { IoCheckmarkDoneSharp } from 'react-icons/io5';
 
 const Voices = () => {
-    const [selectedVoice, setSelectedVoice] = useState(null);
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-      };
-    const voiceDescriptions = {
-        'alena': 'Алёна 💅',
-        'filipp': 'Филипп 👤',
-        'ermil': 'Ермил 👤',
-        'jane': 'Джейн 💅',
-        'madirus': 'Мадирас 👤',
-        'omazh': 'Омаж 👤',
-        'zahar': 'Захар 👤',
-        'dasha': 'Даша 💅',
-        'julia': 'Юлия 💅',
-        'lera': 'Лера 💅',
-        'masha': 'Маша 💅',
-        'marina': 'Марина 💅',
-        'alexander': 'Александр 👤',
-        'kirill': 'Кирилл 👤',
-        'anton': 'Антон 👤'
-    }
-    const handleVoiceSelect = (voice) => {
-        setSelectedVoice(voice);
-        // Здесь вы можете вызвать функцию для отправки выбранного голоса в ваш Python Telegram Bot
-        // Например, можно использовать fetch или axios для отправки запроса на сервер.
-      };
+  const [selectedVoice, setSelectedVoice] = useState(null);
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+  };
+
+  const voiceDescriptions = {
+    alena: 'Алёна 💅',
+    filipp: 'Филипп 👤',
+    ermil: 'Ермил 👤',
+    jane: 'Джейн 💅',
+    madirus: 'Мадирас 👤',
+    omazh: 'Омаж 👤',
+    zahar: 'Захар 👤',
+  };
+
+  const voiceDescriptionsSecond = {
+    dasha: 'Даша 💅',
+    julia: 'Юлия 💅',
+    lera: 'Лера 💅',
+    masha: 'Маша 💅',
+    marina: 'Марина 💅',
+    alexander: 'Александр 👤',
+    kirill: 'Кирилл 👤',
+    anton: 'Антон 👤',
+  };
+
+  const handleVoiceSelect = (voice) => {
+    setSelectedVoice(voice);
+    // Здесь вы можете вызвать функцию для отправки выбранного голоса в ваш Python Telegram Bot
+    // Например, можно использовать fetch или axios для отправки запроса на сервер.
+  };
+
   return (
-    <div>
-    <h2 className= {style.text2}>Выберите голос</h2>
-    <Slider {...settings}>
-     
-      {Object.keys(voiceDescriptions).map((voice) => (
-        <div
-          key={voice}
-          
-          className={`${style.voice}  ${selectedVoice === voice ? 'selected' : ''}` }
-          onClick={() => handleVoiceSelect(voice)}
-        >
-          <p className= {style.text}>{voiceDescriptions[voice]}</p>
-          <div className= {style.btns}>
-            <button><GrCaretNext/></button>
-            <button>
-            <IoCheckmarkDoneSharp />
-
-            </button>
+    <div className={style.top}>
+      <Slider {...settings}>
+        {Object.keys(voiceDescriptions).map((voice) => (
+          <div
+            key={voice}
+            
+            className={`${style.voice} text-2xl  ${selectedVoice === voice ? 'selected' : ''}`}
+            onClick={() => handleVoiceSelect(voice)}
+          >
+            <p className={style.text}>{voiceDescriptions[voice]}</p>
+            <div className={style.btns}>
+              <button>
+                <GrCaretNext />
+              </button>
+              <button>
+                <IoCheckmarkDoneSharp />
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
-    </Slider>
-  </div>
-  )
-}
+        ))}
+      </Slider>
+      <Slider {...settings}>
+        {Object.keys(voiceDescriptionsSecond).map((voice) => (
+          <div
+            key={voice}
+            className={`${style.voice} ${selectedVoice === voice ? 'selected' : ''}`}
+            onClick={() => handleVoiceSelect(voice)}
+          >
+            <p className={style.text}>{voiceDescriptionsSecond[voice]}</p>
+            <div className={style.btns}>
+              <button>
+                <GrCaretNext />
+              </button>
+              <button>
+                <IoCheckmarkDoneSharp />
+              </button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
-export default Voices
+export default Voices;
