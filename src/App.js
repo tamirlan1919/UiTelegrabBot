81e127fa77a035ee45734e44d170a329391b5cf5
Timerlan Chinchaev <tchinchaev@bk.ru>
2024-02-08 22:01:29 +0300

    add tg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Tuning from './components/tuning/Tuning';
 import Voices from './components/voices/Voices';
 
 const tg = window.Telegram.WebApp;
+
 function App() {
+  console.log(tg?.user?.username)
   return (
     <div className="App">
       <Voices/>
