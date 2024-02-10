e702af683a84523a6bf55afa8a2065014e148879
tamirlan1919 <tchinchaev@bk.ru>
2024-02-10 17:26:17 +0300

    remove tg from app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import Tuning from './components/tuning/Tuning';
 import Voices from './components/voices/Voices';
 
-const tg = window.Telegram.WebApp;
 
 function App() {
   // Здесь вы можете получить user_id, например, из localStorage или другого источника
-  const user_id = tg?.initDataUnsafe?.user?.username(); // Примерная функция для получения user_id
+  const user_id = 7777777// Примерная функция для получения user_id
 
   return (
     <div className="App">
